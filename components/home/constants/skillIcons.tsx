b5fa92cdb42394f5a68909b8a35b2e7cd4720d9f
Danjoe4 <djbroderick4@gmail.com>
2022-12-22 19:57:31 -0500

    initial commit

---
+++ components/home/constants/skillIcons.tsx
@@ -0,0 +1,87 @@
+import {
+  SiFlask,
+  SiPython,
+  SiJavascript,
+  SiGit,
+  SiAmazonaws,
+  SiReact,
+  SiGnubash,
+  SiMongodb,
+  SiMaterialui,
+  SiBootstrap,
+  SiNextdotjs,
+  SiPostgresql,
+  SiDjango,
+  SiApache,
+} from 'react-icons/si';
+import { AiOutlineConsoleSql } from 'react-icons/ai';
+import { DiNginx } from 'react-icons/di';
+
+const skillLogos = [
+  {
+    icon: <SiPython title="Python" />,
+    label: 'Python',
+  },
+  {
+    icon: <SiAmazonaws title="Amazonaws" />,
+    label: 'Amazonaws',
+  },
+  {
+    icon: <AiOutlineConsoleSql title="MicrosoftSqlServer" />,
+    label: 'OutlineConsoleSql',
+  },
+  {
+    icon: <SiGit title="Git" />,
+    label: 'Git',
+  },
+  {
+    icon: <SiJavascript title="JavaScript" />,
+    label: 'JavaScript',
+  },
+  {
+    icon: <SiGnubash title="bash" />,
+    label: 'Gnubash',
+  },
+  {
+    icon: <SiFlask title="Flask" />,
+    label: 'Flask',
+  },
+  {
+    icon: <SiDjango title="Django" />,
+    label: 'Django',
+  },
+  {
+    icon: <SiNextdotjs title="NextJS" />,
+    label: 'NextJS',
+  },
+  {
+    icon: <SiPostgresql title="PostgreSQL" />,
+    label: 'PostgreSQL',
+  },
+  {
+    icon: <SiMongodb title="MongoDB" />,
+    label: 'MongoDB',
+  },
+  {
+    icon: <SiReact title="React" />,
+    label: 'React',
+  },
+  {
+    icon: <SiMaterialui title="Material UI" />,
+    label: 'Material UI',
+  },
+  {
+    icon: <SiBootstrap title="Bootstrap" />,
+    label: 'Bootstrap',
+  },
+  {
+    icon: <DiNginx title="Nginx" />,
+    label: 'Nginx',
+  },
+  {
+    icon: <SiApache title="Apache" />,
+    label: 'SiApache',
+  },
+];
+
+export default skillLogos;
